9a010aedf9ea9e0768017c59ac26c850d1d708a4
Esraa M <esraascience1996@gmail.com>
2024-10-03 00:40:17 +0300

    sen email using nodemailer

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,47 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import cors from "cors";
+import nodemailer from "nodemailer";
+import { Request, Response } from "express";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const corsHandler = cors({ origin: true });
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Gmail credentials
+const gmailEmail = 'eslam.faisal.ef@gmail.com';
+const gmailPassword = 'bbwl hzbv locf qzbk';
+
+// Create a transporter object using nodemailer
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+exports.sendEmails = onRequest((req: Request, res: Response) => {
+  corsHandler(req, res, async () => {
+    if (req.method !== 'POST') {
+      res.status(405).send('Method Not Allowed');
+      return;
+    }
+
+    try {
+      const mailOptions = {
+        from: `Your Name <${gmailEmail}>`,
+        to: 'eslamfaisal423@gmail.com', // Dynamic recipient email
+        subject: "title",
+        html: `<p>${'message'}</p>`,
+      };
+
+      await transporter.sendMail(mailOptions);
+      logger.info("Email sent successfully.");
+      res.status(200).send('Email sent successfully!');
+    } catch (error) {
+      logger.error('Error sending email:', error);
+      res.status(500).send('Error sending email');
+    }
+  });
+});
